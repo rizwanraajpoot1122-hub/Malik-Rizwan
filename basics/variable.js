70189a4f8f1ad287f2864518900d0c191a9b9738
rizwanraajpoot1122-hub <rizwanraajpoot1122@gmail.com>
2025-10-30 11:56:09 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 133234234
+let accountEmail = "rizwan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pindi" 
+
+//const change nii hota var hum use nii krty bcz latest version of js main var use nii hota hum only let and const use krtyy hain
+//or hagr ziyada cheezon ka aik sath output lena hoo tu uska easy form ya way console table k ssth liyaa jaa sakta haii jese k nichyy example dii gayii haii
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
